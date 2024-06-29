ae2753eb762fb5c4bb9fecfba7c10f7ff0e06e8c
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-29 08:57:25 +0545

    Create file and route that

--- Blog_Frontend/src/App.jsx
+++ Blog_Frontend/src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Component/Pages/Login.jsx';
+import Nopage from './Component/Pages/Nopage.jsx';
+import Navbar from './Component/Layouts/Navbar.jsx';
+import Register from './Component/Pages/Register.jsx';
+import Sidebar from './Component/Layouts/Sidebar.jsx';
+import Home from './Component/Pages/Home.jsx';
+import Logout from './Component/Pages/Logout.jsx';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="*" element={<Nopage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Blog_Frontend/src/Component/Pages/Logout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <>
+    logout
+    </>
+  )
+}
+
+export default Logout;
