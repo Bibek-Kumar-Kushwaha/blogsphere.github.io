7ce3f5a4753efde1b4bc23c4db11d83fd96edbdd
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-07-11 15:30:58 +0545

    change frontend url

--- Blog_Backend/index.js
+++ Blog_Backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", // Allow requests from your React frontend
+    origin: "https://blogsphere-github-io-zqmc.vercel.app", // Allow requests from your React frontend
     methods: ["GET", "PUT", "DELETE", "POST", "PATCH"],
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
